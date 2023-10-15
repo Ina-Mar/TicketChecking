fde484ac7589bb04e54fd8dc35d781e42667ff5f
Ina-Mar <115873882+Ina-Mar@users.noreply.github.com>
2023-10-15 15:01:21 +0300

    random code

--- src/TicketChecker/ConsoleNumber.java
+++ src/TicketChecker/ConsoleNumber.java
@@ -1,2 +1,53 @@
-package TicketChecker;public class ConsoleNumber {
+package TicketChecker;
+import java.util.Scanner;
+
+//Tikrina įvestą bilieto numerį
+public class ConsoleNumber {
+    public static void main(String[] args) {
+        System.out.println("Įveskite bilieto numerį: ");
+        Scanner scanner = new Scanner(System.in);
+        String in = scanner.nextLine();
+        while (in.length() != 6) {
+            System.out.println("Netinkamas numeris. Bandykite dar kartą: ");
+            in = scanner.nextLine();
+        }
+        int number = Integer.parseInt(in);
+
+        boolean lucky = checkNumber(number);
+        if (lucky) {
+            System.out.println("Bilietas laimingas");
+        }
+       else {
+            System.out.println("Bilietas nelaimingas");
+        }
+
+    }
+    public static boolean checkNumber(int number) {
+        boolean isLucky = false;
+        boolean isUnique = true;
+        int[] digits = new int[6];
+        int index = 0;
+        while (number > 0) {
+            digits[index] = number%10;
+            index++;
+            number /= 10;
+        }
+        for (int i = 0; i < digits.length - 1; i++) {
+            for (int j = i+1; j < digits.length; j++) {
+                if (digits[i] == digits[j]) {
+                    isUnique = false;
+                    break;
+                }
+            }
+        }
+        if (isUnique) {
+            int sum1 = digits[0] + digits[1] + digits[2];
+            int sum2 = digits[3] + digits[4] + digits[5];
+            if (sum1 == sum2) {
+                isLucky = true;
+            }
+        }
+        return isLucky;
+
+    }
 }
--- src/TicketChecker/RandomNumber.java
+++ src/TicketChecker/RandomNumber.java
@@ -1,2 +1,18 @@
-package TicketChecker;public class RandomNumber {
+package TicketChecker;
+import java.util.Random;
+//Generuoja atsitiktinius blietus, kol randa laimingą
+public class RandomNumber {
+    public static void main(String[] args) {
+        boolean isLucky = false;
+        int count = 0;
+        int randNum = 0;
+        while (!isLucky) {
+            randNum = new Random().nextInt(900000) + 100000;
+            isLucky = ConsoleNumber.checkNumber(randNum);
+            count++;
+        }
+
+        System.out.println("Laimingas numeris yra: " + randNum);
+        System.out.println("Bandymų skaičius: " + count);
+    }
 }
